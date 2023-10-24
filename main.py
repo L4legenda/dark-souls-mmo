f0cc20a26a5a484852fb0aab289efb9c8ca0ed46
l4legenda <l4legenda@gmail.com>
2023-10-24 09:31:59 +0500

    render map

--- main.py
+++ main.py
@@ -1,15 +1,29 @@
 import pygame
+import pytmx
+from pytmx.util_pygame import load_pygame
 
 pygame.init()
 screen = pygame.display.set_mode((1000, 800))
+
+def draw_tiled_map(tmxdata, screen):
+    for layer in tmxdata.visible_layers:
+        if isinstance(layer, pytmx.TiledTileLayer):
+            for x, y, gid, in layer:
+                tile = tmxdata.get_tile_image_by_gid(gid)
+                if tile:
+                    screen.blit(tile, (x * tmxdata.tilewidth, y * tmxdata.tileheight))
+
 done = False
 
-x = 0
-y = 0
+x = 500
+y = 400
 
 player = pygame.Rect(x, y, 50, 50)
 
 platform = pygame.Rect(0, 700, 1000, 100)
+platform2 = pygame.Rect(0, 0, 1000, 100)
+platform3 = pygame.Rect(0, 0, 100, 1000)
+platform4 = pygame.Rect(900, 0, 100, 1000)
 
 clock = pygame.time.Clock()
 
@@ -23,26 +37,26 @@ while not done:
     if keypressed[pygame.K_w]:
         phantom_player: pygame.Rect = player.copy()
         phantom_player.y -= 2
-        isCollide = phantom_player.colliderect(platform)
-        if not isCollide:
+        isCollide = phantom_player.collidelist([platform, platform2, platform3, platform4])
+        if isCollide == -1:
             player.y -= 2
     if keypressed[pygame.K_s]:
         phantom_player: pygame.Rect = player.copy()
         phantom_player.y += 2
-        isCollide = phantom_player.colliderect(platform)
-        if not isCollide:
+        isCollide = phantom_player.collidelist([platform, platform2, platform3, platform4])
+        if isCollide == -1:
             player.y += 2
     if keypressed[pygame.K_a]:
         phantom_player: pygame.Rect = player.copy()
         phantom_player.x -= 2
-        isCollide = phantom_player.colliderect(platform)
-        if not isCollide:
+        isCollide = phantom_player.collidelist([platform, platform2, platform3, platform4])
+        if isCollide == -1:
             player.x -= 2
     if keypressed[pygame.K_d]:
         phantom_player: pygame.Rect = player.copy()
         phantom_player.x += 2
-        isCollide = phantom_player.collidelist(platform)
-        if not isCollide:
+        isCollide = phantom_player.collidelist([platform, platform2, platform3, platform4])
+        if isCollide == -1:
             player.x += 2
 
     screen.fill((255, 255, 255))
